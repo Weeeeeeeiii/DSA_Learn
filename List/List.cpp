23db742602b791c9dca96d417b5f01b317d71f36
Jullre <v1waych@foxmail.com>
2024-10-21 20:55:24 +0800

    基本完成对 List.h 的测试

--- List/List.cpp
+++ List/List.cpp
@@ -3,45 +3,86 @@
 
 int main()
 {
-    List<int> lst;
-    for (int i = 0; i < 10; ++i)
-    {
-        lst.push_back(i);
+    // List 初始化
+    List<int> lst_a{1, 2, 3, 4, 5};
+    std::cout << "lst_a 的初始值：" << std::endl;
+    for (auto &i : lst_a) {
+        std::cout << i << "\t";
+    }
+    std::cout << std::endl;
+
+    // 这两个 for 等价
+    std::cout << "lst_a 的初始值：" << std::endl;
+    for (auto iter = lst_a.begin(); iter != lst_a.end(); ++iter) {
+        std::cout << *iter << "\t";
+    }
+    std::cout << std::endl;
+
+    // push 测试
+    List<int> lst_b;
+    std::cout << "lst_b 从后 push 进 1 到 10 后：" << std::endl;
+    for (int i = 1; i <= 10; ++i) {
+        lst_b.push_back(i);
     }
 
-    lst.pop_back();
-
-    for (auto it : lst)
-    {
-        std::cout << it << std::endl;
+    for (auto &i : lst_b) {
+        std::cout << i << "\t";
     }
+    std::cout << std::endl;
 
-    // for (auto it = lst.begin(); it != lst.end(); ++it)
-    // {
-    //     std::cout << *it << std::endl;
-    // }
+    std::cout << "lst_b 再从前 push 进 1 到 10 后：" << std::endl;
+    for (int i = 1; i <= 10; ++i) {
+        lst_b.push_front(i);
+    }
+    for (auto &i : lst_b) {
+        std::cout << i << "\t";
+    }
+    std::cout << std::endl;
+    
+    // 拷贝构造函数
+    List<int> lst_c = lst_b;
+    std::cout << "lst_c 从 lst_b 拷贝过来：" << std::endl;
+    for (auto &i : lst_c) {
+        std::cout << i << "\t";
+    }
+    std::cout << std::endl;
 
-//     List<int> lst = {1, 2, 3, 4, 5};
-//     for (auto &x : lst)
-//     {
-//         std::cout << x << "\t";
-//     }
-//     std::cout << std::endl;
+    // 输出 List 大小
+    std::cout << "lst_c 此时的大小为：" << lst_c.size() << "\t";
+    std::cout << std::endl;
 
-// //    List<int> lst2 = std::move(lst);
-//     List<int> lst2 = List<int> {5, 6};
-//     for (auto &x : lst2)
-//     {
-//         std::cout << x << "\t";
-//     }
-//     std::cout << std::endl;
+    // pop 测试
+    for (int i = 1; i<= 5; ++i) {
+        lst_b.pop_back();
+    }
+    std::cout << "lst_b 从后 pop 出 5 个元素：" << std::endl; 
+    for (auto &i : lst_b) {
+        std::cout << i << "\t";
+    }
+    std::cout << std::endl;
 
-//     lst2 = std::move(lst);
-//     for (auto &x : lst2)
-//     {
-//         std::cout << x << "\t";
-//     }
-//     std::cout << std::endl;
+    for (int i = 1; i<= 10; ++i) {
+        lst_c.pop_front();
+    }
+    std::cout << "lst_c 从前 pop 出 5 个元素：" << std::endl;
+    for (auto &i : lst_c) {
+        std::cout << i << "\t";
+    }
+    std::cout << std::endl;
+
+    // 自增自减和动态迭代器测试
+    std::cout << "lst_a 全部元素改为 10086：" << std::endl;
+    for (auto iter = lst_a.begin(); iter != lst_a.end(); ++iter) {
+        *iter = 10086;
+    }
+    for (auto &i : lst_a) {
+        std::cout << i << "\t";
+    }
+    std::cout << std::endl;
+    auto iter = lst_c.begin();
+    std::cout << "iter 指向 lst_c 的 begin(), 依次输出 iter++, ++iter, iter--, --iter 指向的值" << std::endl;
+    std::cout << *(iter++) << '\t' << *(++iter) << '\t';
+    std::cout << *(iter--) << '\t' << *(--iter) << std::endl;
 
     return 0;
 }
