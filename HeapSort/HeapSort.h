62724fc3a2bf5dfb65edd12c1cbf4be14afbdadd
Jullre <v1waych@foxmail.com>
2024-11-28 22:40:09 +0800

    Repair some bugs

--- HeapSort/HeapSort.h
+++ HeapSort/HeapSort.h
@@ -19,11 +19,11 @@ void Heapify(std::vector<T> &array, std::size_t length, std::size_t index)
     auto right_child{left_child + 1};
     auto largest{index};
 
-    if (left_child < length && array[left_child] < array[index])
+    if (left_child < length && array[left_child] > array[largest])
     {
         largest = left_child;
     }
-    if (right_child < length && array[right_child] < array[index])
+    if (right_child < length && array[right_child] > array[largest])
     {
         largest = right_child;
     }
@@ -31,7 +31,7 @@ void Heapify(std::vector<T> &array, std::size_t length, std::size_t index)
     if (largest != index)
     {
         std::swap(array[index], array[largest]);
-        Heapify(array, largest);
+        Heapify(array, length, largest);
     }
 }
 
@@ -44,8 +44,8 @@ void SortHeap(std::vector<T> &array)
 {
     for (auto length{std::ssize(array) - 1}; length > 0; --length)
     {
-        std::swap(array[0], array[length]);
-        Heapify(array, length, 0);
+        std::swap(array.data()[0], array.data()[length]);
+        Heapify(array, static_cast<std::size_t> (length), 0);
     }
 }
 #endif
